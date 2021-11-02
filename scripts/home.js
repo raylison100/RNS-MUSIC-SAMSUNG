dea3da1d1facf481cc35b622d19e39c4db581eb7
raylison100 <raylison100@gmail.com>
2021-11-01 21:35:27 -0300

    ajsutes focos e animações

--- scripts/home.js
+++ scripts/home.js
@@ -9,17 +9,13 @@ for(var i=0; i<20; i++){
 	console.log(i);
 }
 
-$('#recomendation-list').caphList({
+$('#recomendationList').caphList({
 	items : items,
 	template: 'template1',
+	containerClass: 'list-group',
+	wrapperClass: 'wrapper',
 
-	onFocused: function (event) {
-		$(event.currentTarget).css({
-			'background-color': '#a71717',
-	 	});
-	 console.log("focou item")
- 	},
-	 
+
 	onFocusItemView: function(context) {
 		console.log('focus', context);
 		context.onFocused
@@ -37,3 +33,76 @@ $('#recomendation-list').caphList({
 		console.log('scroll finish!', context);
 	}
 });
+
+$('#newsList').caphList({
+	items : items,
+	template: 'template1',
+
+	onFocusItemView: function(context) {
+		console.log('focus', context);
+		context.onFocused
+	},
+	onReachStart: function(context) {
+		console.log('reach start', context);
+	},
+	onReachEnd: function(context) {
+		console.log('reach end', context);
+	},
+	onScrollStart: function(context) {
+		console.log('scroll start!', context);
+	},
+	onScrollFinish: function(context) {
+		console.log('scroll finish!', context);
+	}
+});
+
+$('#popularList').caphList({
+	items : items,
+	template: 'template1',
+
+	onFocusItemView: function(context) {
+		console.log('focus', context);
+		context.onFocused
+	},
+	onReachStart: function(context) {
+		console.log('reach start', context);
+	},
+	onReachEnd: function(context) {
+		console.log('reach end', context);
+	},
+	onScrollStart: function(context) {
+		console.log('scroll start!', context);
+	},
+	onScrollFinish: function(context) {
+		console.log('scroll finish!', context);
+	}
+});
+
+$('.item-blocos-musics').caphButton({
+    onFocused: function (event) {
+		$(event.currentTarget).find('div').css({
+			'transform': 'scale(1.2)',
+			'background-color': '#FFF',
+        });
+        console.log("focou item")
+    },
+    onBlurred: function (event) {
+		$(event.currentTarget).find('div').css({
+            'transform': 'scale(1)',
+			'background-color': '#000',
+        });
+        console.log("focou item")
+    },
+    toggle: true,
+    onSelected: function (event, selected) {
+	     location.href = "../pages/musics.html";
+         console.log("clicou item")
+    }
+});
+
+
+
+$('.blocos-musics').css({
+		'width': '180px',
+		'height' : '180px',
+})
--- scripts/nav.js
+++ scripts/nav.js
@@ -33,14 +33,14 @@ $('#my-list').caphButton({
 	   	    'background-color': '#a71717',
 	   		'color': '#FFF'
 	    });
-        console.log("focou favoritos")
+        console.log("focou list")
     },
     onBlurred: function (event) {
 	   	$(event.currentTarget).css({
 	   	    'background-color': '#aaa3a3',
 	   		'color': 'rgb(78, 72, 72)'
 	    });
-        console.log("desfocou home")
+        console.log("desfocou list")
     },
     focusOption: {
         name: "list",
@@ -52,7 +52,7 @@ $('#my-list').caphButton({
     toggle: true,
     onSelected: function (event, originalEvent, selected) {
 	    location.href = "../pages/my-list.html";
-        console.log("clicou home")
+        console.log("clicou list")
     }
 });
 
@@ -82,6 +82,34 @@ $('#search').caphButton({
     onSelected: function (event, originalEvent, selected) {
 	     console.log("opa")
 	     location.href = "../pages/search.html";
-         console.log("clicou home")
+         console.log("clicou search")
+    }
+});
+
+$('#account-icon').caphButton({
+    onFocused: function (event) {
+        $(event.currentTarget).closest('div').css({
+            'transform': 'scale(1.3)'
+        });
+        console.log("focou account")
+    },
+    onBlurred: function (event) {
+        $(event.currentTarget).closest('div').css({
+            'transform': 'scale(1)'
+        });
+        console.log("desfocou account")
+    },
+    focusOption: {
+        name: "account",
+        depth: 0,
+        group: "menu",
+        disabled: false,
+        initialFocus: false
+    },
+    toggle: true,
+    onSelected: function (event, originalEvent, selected) {
+	     console.log("opa")
+	     location.href = "../index.html";
+         console.log("clicou account")
     }
 });
